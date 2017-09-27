82f272493ec0696a65ebde8b305dd0f0d3f255f8
luosader <greatsen@sina.com>
2017-09-27 13:53:47 +0800

    4

--- index.php
+++ index.php
@@ -44,6 +44,7 @@ $smarty->assign('show_list2', $dou->get_show_list('center'));
 // $smarty->assign('link', $dou->get_link_list());
 $smarty->assign('index', $index);
 // $smarty->assign('recommend_product', $dou->get_list('product', 'ALL', $_DISPLAY['home_product'], 'sort DESC'));
+$smarty->assign('ra_cat_name',$dou->get_one("SELECT cat_name from ".$dou->table('article_category')." where cat_id=".$recommend_article_ID));
 $smarty->assign('recommend_article', $dou->get_list('article', $recommend_article_ID, $_DISPLAY['home_article'], 'sort,add_time desc'));
 $smarty->assign('article_activity', $dou->get_list('article', $article_activity_ID, $_DISPLAY['home_article'], 'sort,add_time desc'));
 
